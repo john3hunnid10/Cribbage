cf6c97570d9c0aee88a693dc12a8d3070b8ac13f
john3hunnid10 <johnadamsnyc@gmail.com>
2024-09-11 20:01:23 -0400

    basic outline of deck&card class

--- deckObject.py
+++ deckObject.py
@@ -1,2 +1,19 @@
 #deck Object
 import random
+
+
+#card class
+class Card:
+    def __init__(self,suit,rank,value):
+        self.suit=suit
+        self.rank=rank
+        self.value=value
+
+class Deck:
+    suits=['H','D','C','S']
+    rankValTuple=[
+        ('A',1),('2',2),('3',3),('4',4),('5',5),('6',6),('7',7),
+        ('8',8),('9',9),('10',10),('J',10),('Q',10),('K',10)]
+    def __init__(self):
+        self.cards=[Card(suit,rank,value) for suit in self.suits for rank,value in self.rankValTuple]
+    
