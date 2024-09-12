3d8660ebcc897426693bbe7f3b764763082d4410
john3hunnid10 <johnadamsnyc@gmail.com>
2024-09-11 20:31:00 -0400

    added comments

--- deckObject.py
+++ deckObject.py
@@ -10,22 +10,21 @@ class Card:
         self.value=value
 
 class Deck:
+    #available suits H=hearts, D=diamonds, C=clubs, S=spades 
     suits=['H','D','C','S']
-    
+    #rankValTuple has each rank of card and its corresponding value
     rankValTuple=[
         ('A',1),('2',2),('3',3),('4',4),('5',5),('6',6),('7',7),
         ('8',8),('9',9),('10',10),('J',10),('Q',10),('K',10)]
-    
+    #The deck is constructed with every combination of suit & rank, and the value is added to card object as well
     def __init__(self):
         self.cards=[Card(suit,rank,value) for suit in self.suits for rank,value in self.rankValTuple]
     
-    def __len__(self):
-        return len(self.cards)
-    
+    #shuffles the deck
     def shuffle(self):
         random.shuffle(self.cards)
     
-    
+    #Note: always shuffle before deal 
     def deal(self, numCards):
         deal=self.cards[:numCards]
         self.cards=self.cards[numCards:]
